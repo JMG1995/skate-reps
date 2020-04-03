cf94165fedc89b0b37d6a9e990afc374cc919d4b
Joseph <jmgrant1995@gmail.com>
2020-04-03 12:23:14 +0800

    Add title and some heading spacing fixes

--- src/components/header.js
+++ src/components/header.js
@@ -1,26 +1,13 @@
-import { graphql, useStaticQuery, Link } from 'gatsby';
+import { Link } from 'gatsby';
 import React from 'react';
 import HomeIcon from '../images/home_icon.svg';
 
 function Header() {
-	const { site } = useStaticQuery(graphql`
-		query SiteTitleQuery {
-			site {
-				siteMetadata {
-					title
-				}
-			}
-		}
-	`);
-
 	return (
 		<header className="bg-crooked-purple">
-			<div className="flex flex-wrap items-center justify-end max-w-4xl mx-auto p-4">
+			<div className="flex flex-wrap items-center justify-end max-w-sm lg:max-w-4xl mx-auto p-4">
 				<Link className="flex items-center no-underline text-white" to="/">
-					<img src={HomeIcon} />
-					<span className="hidden font-bold text-xl tracking-tight">
-						{site.siteMetadata.title}
-					</span>
+					<img src={HomeIcon} alt="Home icon" />
 				</Link>
 			</div>
 		</header>

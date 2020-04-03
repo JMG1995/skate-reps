5f07a3ac58d9ea0cdb629d971cd149ab2281ddc3
Joseph <jmgrant1995@gmail.com>
2020-04-03 12:06:46 +0800

    Setup a bunch of defaults and custom tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
-  theme: {},
-  variants: {},
-  // https://github.com/tailwindcss/custom-forms
-  plugins: [require("@tailwindcss/custom-forms")]
+	theme: {
+		colors: {
+			black: '#161616',
+			white: '#f6f6f6',
+			'crooked-purple': '#4C52E8',
+			'flow-green': '#46D66D',
+			'am-blue': '#31CDD3',
+			'pro-red': '#E65353',
+		},
+		fontFamily: {
+			default: ['Helvetica Neue'],
+			montserrat: ['Montserrat'],
+		},
+	},
+	variants: {},
+	// https://github.com/tailwindcss/custom-forms
+	plugins: [require('@tailwindcss/custom-forms')],
 };
